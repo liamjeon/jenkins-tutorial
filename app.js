b4229b051be7423a32b40e34ac13753a0e310be9
liamjeon <89363456+liamjeon@users.noreply.github.com>
2022-01-17 10:32:35 +0900

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+
+app.listen(PORT,HOST,() => {
+    console.log(`Server Listening on ${HOST}:${PORT}`);
+})
+
+module.exports = app;
